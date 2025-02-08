182bed90ae15bda283c24a9dcbd1143fb9594721
shresth saxena <shresth@newme.asia>
2025-02-08 18:54:39 +0530

    ss

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+load_dotenv() 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'leaderboard.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgres://neondb_owner:npg_PXQ0f5hsxudw@ep-ancient-violet-a44bf5fj-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.urandom(24) 
+    
