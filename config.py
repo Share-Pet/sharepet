c10ea6ef29680925b4ab83eec7ca3c7b6db7827a
shresth <shresth@newme.asia>
2025-08-09 17:13:14 +0530

    ss

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 from datetime import timedelta
 from dotenv import load_dotenv
+from sqlalchemy.pool import NullPool
 
 load_dotenv()
 
@@ -31,8 +32,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI =_normalize_db_url(os.environ.get('DATABASE_URL') or 'sqlite:///pet_community.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_size': 10,
-        'pool_recycle': 3600,
+        'pool_size': NullPool,
         'pool_pre_ping': True
     }
     
