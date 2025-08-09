9cc7ff2a88a74dca8516e807c40cbfab098b8a53
shresth <shresth@newme.asia>
2025-08-09 18:39:36 +0530

    ss

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,6 @@ from flask_jwt_extended import (
 )
 from datetime import datetime
 import os
-import  sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "backend"))
-
 from dotenv import load_dotenv
 
 from config import Config
